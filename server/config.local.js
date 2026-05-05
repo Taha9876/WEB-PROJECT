e55766b1a141daaf466bd915e4b0a34408fcf79f
Taha 9876 <tahayasir98765432@gmail.com>
2026-05-05 21:12:34 +0500

    Updated files

---
+++ server/config.local.js
@@ -0,0 +1,9 @@
+// Server configuration - DO NOT commit this file to version control
+module.exports = {
+  PORT: 5000,
+  SUPABASE_URL: 'https://mnxuktqcqpcitynnbwhw.supabase.co',
+  SUPABASE_SERVICE_ROLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ueHVrdHFjcXBjaXR5bm5id2h3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDc4MjgxNywiZXhwIjoyMDkwMzU4ODE3fQ.OyjquA4c-2PZhjskkxi0k6PLODiDJFki4JzemIcIJ0s',
+ADMIN_EMAIL: 'tahayasir384@gmail.com',
+  GMAIL_USER: 'tahayasir384@gmail.com',
+  GMAIL_APP_PASSWORD: 'wqcoxnjllrpnxqdj',
+};
